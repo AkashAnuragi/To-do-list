80a332cd4f9d3eef7c07185d65bb4bb9a5cd0568
Akash Anuragi <akashanuragi543@gmail.com>
2023-08-23 19:06:31 +0530

    Second Commit

---
+++ calculator/script.js
@@ -0,0 +1,27 @@
+// This function clear all the values
+function clearScreen() {
+    document.getElementById("result").value = "";
+}
+ 
+// This function display values
+function display(value) {
+    document.getElementById("result").value += value;
+}
+ 
+// This function evaluates the expression and returns result
+function calculate() {
+    var p = document.getElementById("result").value;
+    var q = eval(p);
+    document.getElementById("result").value = q;
+}
+
+function clearScreen() {
+    document.getElementById("result").value = "";
+}
+
+function calculate() {
+    var p = document.getElementById("result").value;
+    var q = eval(p);
+    document.getElementById("result").value = q;
+}
+
